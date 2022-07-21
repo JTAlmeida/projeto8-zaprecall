f7863f3213d7c60d40532750164a03af53253ead
Jonatas <jonatasalmeidasm@gmail.com>
2022-07-21 16:52:44 -0300

    all layout done && turn cards working (not as it should be on the final stage yet)

--- src/Components/Firstquestion.js
+++ src/Components/Firstquestion.js
@@ -1,5 +1,23 @@
-export default function Firstquestion(){
-    return (
-        <></>
-    )
+import React from "react";
+
+export default function Firstquestion(props) {
+  const [screen, setScreen] = React.useState(true);
+
+  return (
+    <>
+      {screen ? (
+        <div className="question-turned">
+          <h3>{props.question}</h3>
+          <img src="assets/setinha.png" onClick={() => setScreen(!screen)}></img>
+        </div>
+      ) : (
+        <div className="question-turned">
+          <h3>{props.answer}</h3>
+          <button className="red-button">Não lembrei</button>
+          <button className="orange-button">Quase não lembrei</button>
+          <button className="green-button">Zap!</button>
+        </div>
+      )}
+    </>
+  );
 }
--- src/Components/Questions.js
+++ src/Components/Questions.js
@@ -2,46 +2,79 @@ import React from "react";
 import Firstquestion from "./Firstquestion";
 
 const questions = [
-    { question: "O que é JSX?", answer: "Uma extensão de linguagem do JavaScript." }, 
-    { question: "O React é __", answer: "uma biblioteca JavaScript para construção de interfaces." }, 
-    { question: "Componentes devem iniciar com __", answer: "letra maiúscula." }, 
-    { question: "Podemos colocar __ dentro do JSX.", answer: "expressões" },
-    { question: "O ReactDOM nos ajuda __", answer: "interagindo com a DOM para colocar componentes React na mesma." }, 
-    { question: "Usamos o npm para __", answer: "gerenciar os pacotes necessários e suas dependências." }, 
-    { question: "Usamos props para __", answer: "passar diferentes informações para componentes." }, 
-    { question: "Usamos estado (state) para __", answer: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente." },
-]
-
-const selectedQuestions = [];
+  {
+    Q: "O que é JSX?",
+    A: "Uma extensão de linguagem do JavaScript.",
+  },
+  {
+    Q: "O React é __",
+    A: "uma biblioteca JavaScript para construção de interfaces.",
+  },
+  { Q: "Componentes devem iniciar com __", A: "letra maiúscula." },
+  { Q: "Podemos colocar __ dentro do JSX.", A: "expressões" },
+  {
+    Q: "O ReactDOM nos ajuda __",
+    A: "interagindo com a DOM para colocar componentes React na mesma.",
+  },
+  {
+    Q: "Usamos o npm para __",
+    A: "gerenciar os pacotes necessários e suas dependências.",
+  },
+  {
+    Q: "Usamos props para __",
+    A: "passar diferentes informações para componentes.",
+  },
+  {
+    Q: "Usamos estado (state) para __",
+    A:
+      "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente.",
+  },
+];
 
 const copyQuestions = [...questions];
 
-for (let i = copyQuestions.length - 1; i > 0; i--) {
+const selectedQuestions = [];
+
+(function shuffleFlashcards() {
+  for (let i = copyQuestions.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
     [copyQuestions[i], copyQuestions[j]] = [copyQuestions[j], copyQuestions[i]];
   }
-
-for (let i = 0; i<4; i++){
-    selectedQuestions.push(copyQuestions[i])
-}
+  for (let i = 0; i < 4; i++) {
+    selectedQuestions.push(copyQuestions[i]);
+  }
+})();
+console.log(selectedQuestions);
 
 export default function Questions() {
   return (
     <div className="questions">
       {selectedQuestions.map((question, index) => (
-        <Question key={index} id={index+1} />
+        <Question
+          key={index}
+          id={index + 1}
+          question={question.Q}
+          answer={question.A}
+        />
       ))}
     </div>
   );
 }
 
-function Question({ id }) {
-    const [click, setClick] = React.useState(false);
+
+function Question(props) {
+  const [screen, setScreen] = React.useState(true);
 
   return (
-    <div className="question">
-      <h3>Pergunta {id}</h3>
-      <img src="assets/play.png"></img>
-    </div>
+    <>
+      {screen ? (
+        <div className="question">
+          <h3>Pergunta {props.id}</h3>
+          <img src="assets/play.png" onClick={() => setScreen(!screen)}></img>
+        </div>
+      ) : (
+        <Firstquestion question={props.question} answer={props.answer}/>
+      )}
+    </>
   );
 }
